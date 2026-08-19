c59df115a0de6bb88fe77bab5555f586fc1edfb4
Hazel Joyce Maneja <hazelmaneja18@gmail.com>
2026-08-19 11:45:06 -0400

    Update data-55.02.04.js

--- data-55.02.04.js
+++ data-55.02.04.js
@@ -1,5 +1,5 @@
-const BASE_55_02_02_TO_55_02_03_DATA = {
-  version: "Base 55.02.02 to 55.02.03",
+const BASE_55_02_04_DATA = {
+  version: "Base 55.02.04",
   generalNote: "In 55.00.01, Shell support for Invenco dispensers was classified as Beta (No). This was upgraded to fully supported (Yes) in version 55.01.01+",
   brands: {
     "Bennett": {
